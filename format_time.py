b46113064a95429f186d4df56372ac9c3581af18
StasenkoAV <jahdie@yandex.ru>
2020-06-12 11:26:17 +0700

    Первый код с ошибками. Этот исправленный.

--- format_time.py
+++ format_time.py
@@ -3,13 +3,13 @@ hours = seconds_enter // 3600
 minuts = (seconds_enter // 60) % 60
 seconds = seconds_enter % 60
 if hours < 10:
-    h = '0' + str(hours)
+    hours = '0' + str(hours)
 else:
     hours = str(hours)
 if minuts < 10:
     minuts = '0' + str(minuts)
 else:
-    m = str(minuts)
+    minuts = str(minuts)
 if seconds < 10:
     seconds = "0" + str(seconds)
 else:
